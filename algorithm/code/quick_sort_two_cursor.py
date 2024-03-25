671d28f30f892dc5efdace393b502354de3afe37
bada308 <203190@jnu.ac.kr>
2024-03-25 22:36:47 +0900

    fix: qsort 추상화

--- algorithm/code/quick_sort_two_cursor.py
+++ algorithm/code/quick_sort_two_cursor.py
@@ -14,7 +14,9 @@ def qsort(arr, left, right):
     if left < pr: qsort(arr, left, pr)
     if pl < right: qsort(arr, pl, right)
 
+def quick_sort(arr):
+    qsort(arr, 0, len(arr) - 1)
 
 target = [3, 23, 4, 46, 19, 23, 50, 33]
-qsort(target, 0, len(target) - 1)
+quick_sort(target)
 print(target)
